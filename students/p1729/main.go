def418a75b8dc727db0384595da8f2d3a55a600c
p1729 <pankajkumar1729@gmail.com>
2022-08-20 20:37:54 +0530

    add args parsing

--- students/p1729/main.go
+++ students/p1729/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var flag string
+	if len(os.Args[1:]) == 0 {
+		flag = ""
+	} else {
+		flag = os.Args[1]
+	}
+
+	switch flag {
+	case "-h":
+		fmt.Println("Usage of quiz")
+		fmt.Println("\t-csv string")
+		fmt.Println("\t\ta csv file in the format of 'question,answer' (default \"problems.csv\")")
+	case "-csv":
+		if len(os.Args[2:]) == 0 {
+			fmt.Println("Please provide the filename")
+		} else {
+			fmt.Println("You provided filename", os.Args[2])
+		}
+	default:
+		fmt.Println("Default filename is problems.csv")
+	}
 }
